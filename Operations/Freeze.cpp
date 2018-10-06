f7dd332c71030f50f2f6f039217f9d4fed4d8140
Melissa Gramajo <melissa.gramajo@servicenow.com>
2018-10-06 16:19:35 -0400

    transfer pseudo to C++

--- Operations/Freeze.cpp
+++ Operations/Freeze.cpp
@@ -2,25 +2,52 @@
 //This is important for expanding a narrow ANode.
 
 
-def completeExpansion(en: ENode) = freeze(en.narrow)
-var wide = new Array <Any >(16) copy(en.narrow, wide, en.level) if (!CAS(en.wide, null, wide))
-wide = READ(en.wide) CAS(en.parent[en.parentpos], en, wide)
-def freeze(cur: ANode) = vari=0
-141
-while (i < val node if (node
-cur.length)
-= READ(cur[i]) == null)
-if (!CAS(cur[i], node, FVNode)) i -= 1 else if (node ∈ SNode)
-val txn = READ(node.txn) if (txn == NoTxn)
-if (!CAS(node.txn, NoTxn, FSNode)) i -= 1 else if (txn 􏰀 FSNode)
-CAS(cur[i], node , txn)
-i -= 1
-else if (node ∈ ANode)
-val fn = new FNode(node) CAS(cur[i], node , fn)
-i -= 1
-else if (node ∈ FNode) freeze(node.frozen) else if (node ∈ ENode)
-completeExpansion(node)
-i -= 1 i += 1
+def completeExpansion(ENode en) { 
+    freeze(en.narrow);
+    ANode wide = new Array <Any >(16) copy(en.narrow, wide, en.level);
+    if (!atomic_compare_exchange(en.wide, NULL, wide)) {
+        wide = READ(en.wide);
+    }
+    atomic_compare_exchange(en.parent[en.parentpos], en, wide);
+}
+
+def freeze(ANode cur) {
+    int i = 0;
+    while (i < cur.length){ 
+        ANode node = *cur.wide[cur[i]]; //READ(cur[i]);
+        nodeType = typeid(node).name();
+        if(nodeType == NULL) {
+            if (!atomic_compare_exchange(cur[i], node, FVNode)){
+                i -= 1;
+            }
+        } 
+        else if (nodeType == typeid(SNode).name()){
+            Txn txn = READ(node.txn); //txn only in Snode
+            if (txn == NoTxn) {
+                if (!atomic_compare_exchange(node.txn, NoTxn, FSNode)){
+                    i -= 1;
+                } 
+            }
+            else if (txn 􏰀!= FSNode){
+                atomic_compare_exchange(cur[i], node , txn);
+                i -= 1;
+            }
+        }
+        else if (nodeType == typeid(ANode).name()){
+            val fn = new FNode(node);
+            compare_exchange_weak(cur[i], node , fn);
+            i -= 1;
+        }
+        else if (nodeType == typeid(FNode).name()) {
+            freeze(node.frozen);
+         } 
+         else if (nodeType == typeid(ENode).name()){
+            completeExpansion(node)
+            i -= 1; 
+         }
+        i += 1;
+    }
+} //end of freeze
 
 
 int main(int argc, char** argv)
