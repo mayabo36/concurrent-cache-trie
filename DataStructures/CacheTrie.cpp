53c2e72ec866d60f846bba79eff20e2d149d1109
Rebeca Amaya <rebeca_amaya@bellsouth.net>
2018-11-11 14:09:20 -0500

    semi-working code for insert

--- DataStructures/CacheTrie.cpp
+++ DataStructures/CacheTrie.cpp
@@ -17,33 +17,34 @@ ANode * root;
 
 // Key is the word being inserted
 bool insert(std::string value, std::size_t hash, int level, ANode *& current, ANode *& previous) {
+    
+    int position = (hash >> (level)) % ((current->isWide ? 16 : 4));
 
-    int position;
-    if (current->isWide) position = hash % 16;
-    else position = hash % 4;
+    std::cout << "performing an insert on '" << value << "'" << "at pos " << position << std::endl;
 
-    AnyNode* old = current->wide[position];
+    AnyNode* old;
+    if (current->isWide) old = current->wide[position];
+    else old = current->narrow[position];
 
-    // Check if the location is empty, if so insert the node there
+    // Check if the position is empty, if so insert the node there
     if (old == 0) {
 
-        // // Setup node to be inserted
-        // SNode snode = SNode();
-        // snode.hash = hash;
-        // snode.value = value;
-
         AnyNode* newNode = new AnyNode;
-        newNode->snode = SNode();
         newNode->snode.hash = hash;
         newNode->snode.value = value;
         newNode->nodeType = SNODE;
 
-        if (current->wide[position].compare_exchange_weak(old, newNode)) return true;
+        if(current->isWide){
+            if (current->wide[position].compare_exchange_weak(old, newNode)) return true;
+            else return insert(value, hash, level, current, previous);
+        } else {
+            if (current->narrow[position].compare_exchange_weak(old, newNode)) return true;
+            else return insert(value, hash, level, current, previous);
+        }
 
-        else return insert(value, hash, level, current, previous);
     }
 
-    // Check if the location is occupied by an ANode
+    // Check if the position is occupied by an ANode
     else if (old->nodeType == ANODE) {
         ANode* oldANode = &old->anode;
         return insert(value, hash, level + 4, oldANode, current); // May break everything, needs testing
@@ -55,24 +56,64 @@ bool insert(std::string value, std::size_t hash, int level, ANode *& current, AN
 
         if (txn == NoTxn) {
             if (old->snode.value == value) {
-                // SNode snode = SNode();
-                // snode.hash = hash;
-                // snode.value = value;
 
                 AnyNode* newNode = new AnyNode;
-                newNode->snode = SNode();
                 newNode->snode.hash = hash;
                 newNode->snode.value = value;
                 newNode->nodeType = SNODE;
+                
+                if (newNode->snode.txn.compare_exchange_weak(txn, txn)){ // Making sure NoTxn
+                    current->wide[position].compare_exchange_weak(old, newNode);
+                    return true;
+                }
+                else {
+                    return insert(value, hash, level, current, previous);
+                }
+            } else if (!current->isWide) {
+                int previousPos = (hash >> (level - 4)) * ((previous->isWide ? 16 : 4) - 1);
 
-                // Make Txn in snode atomic
-                // CAS on the txn to announce the snode
-                // then CAS on the AnyNode?
+                // TODO
+                std::cout << "Prev pos: " << previousPos << std::endl;
+            } else {
+                AnyNode* newNode = new AnyNode;
+                newNode->anode.level = level + 4;
+                newNode->nodeType = ANODE;
 
-                //if (current->wide[position].compare_exchange_weak(old, newNode)) return true;
+                AnyNode* temp;
+ 
+                // Insert previous snode into new anode
+                AnyNode* snode1 = new AnyNode;
+                snode1->snode.hash = old->snode.hash;
+                snode1->snode.value = old->snode.value;
+                snode1->nodeType = SNODE;
+                newNode->anode.narrow[old->snode.hash % 4].compare_exchange_weak(temp, snode1);
+
+                // Insert new snode into new anode
+                AnyNode* snode2 = new AnyNode;
+                snode2->snode.hash = hash;
+                snode2->snode.value = value;
+                snode2->nodeType = SNODE;
+                newNode->anode.narrow[hash % 4].compare_exchange_weak(temp, snode2);
+
+                if (old->snode.txn.compare_exchange_weak(txn, txn)) {
+                    if (current->isWide) {
+                        current->wide[position].compare_exchange_weak(old, newNode);
+                    } else {
+                        current->narrow[position].compare_exchange_weak(old, newNode);
+                    }
+                    return true;
+                } else {
+                    return insert(value, hash, level, current, previous);
+                }
             }
+        } 
+        // SNode is frozen
+        else if (txn == FSNode) {
+            return false;
         }
-        return true;
+        else { // Otherwise help finish the operation that another thread is performing and try again
+            return insert(value, hash, level, current, previous);
+        }
     }
 
     // Check if the position is occupied an ENode
@@ -85,8 +126,6 @@ bool insert(std::string value, std::size_t hash, int level, ANode *& current, AN
 }
 
 bool insert(std::string value){
-    std::cout << "performing an insert on '" << value << "'" << std::endl;
-
     ANode* node = NULL;
 
     if ( !insert(value, std::hash<std::string>{}(value), 0, root, node) )
@@ -115,25 +154,44 @@ bool search(ANode root, std::string key) {
 
 }
 
-int main() {
-    std::string values[] = {"melissa", "emily", "ashton", "rebeca"};
-    int n = sizeof(values) / sizeof(values[0]);
+void printTree(ANode* anode) {
 
-    root = new ANode;
+    int length = (anode->isWide ? 16 : 4);
 
-    for(int i = 0; i < n; i++) {
-        insert( values[i]);
-    }
+    for(int i = 0; i < length; i++) {
 
-    ANode* tempRoot = root;
-    for(int i = 0; i < 16; i++) {
-        AnyNode* node = tempRoot->wide[i];
+        AnyNode* node;
+        if (anode->isWide) {node = anode->wide[i];} 
+        else node = anode->narrow[i];
+
         if(node != 0){
-            if(node->nodeType == SNODE) {
-                std::cout << node->snode.value << " at location " << i << std::endl;
+            switch(node->nodeType) {
+                case SNODE:
+                    std::cout << node->snode.value << " at location " << i << std::endl;
+                    break;
+                case ANODE:
+                    std::cout << "Traverse anode at " << i << std::endl;
+                    printTree(&node->anode);
+                    break;
             }
         }
     }
+}
+
+int main() {
+    std::string values[] = {"melissa", "emily", "ashton", "rebeca", "damian", "victor", "tyler", "pichi", "pom", "neo", "precious"};
+    int n = sizeof(values) / sizeof(values[0]);
+
+    root = new ANode;
+    root->isWide = true;
+
+    for(int i = 0; i < n; i++) {
+        //insert(std::rand());
+        insert(values[i]);
+    }
+
+    ANode* tempRoot = root;
+    printTree(tempRoot);
 
     return 0;
 }
--- Nodes/SNode.cpp
+++ Nodes/SNode.cpp
@@ -11,8 +11,21 @@ SNode::SNode() {
     //SNode::txn = NoTxn;
 }
 
-void SNode::swap(SNode& first, SNode& second) {
-    std::swap(first.hash, second.hash);
-    std::swap(first.value, second.value);
-    std::swap(first.txn, second.txn);
-}
+// SNode::SNode(std::size_t hash, std::string value, Txn txn) {
+//     this->hash = hash;
+//     this->value = value;
+//     std::atomic<Txn> t {txn};
+//     std::swap(this->txn, t);
+// }
+
+// SNode::SNode(const SNode & node) {
+//     // this->hash = node.hash;
+//     // this->value = node.value;
+//     // this->txn = node.txn;
+// }
+
+// void SNode::swap(SNode& first, SNode& second) {
+//     std::swap(first.hash, second.hash);
+//     std::swap(first.value, second.value);
+//     std::swap(first.txn, second.txn);
+// }
